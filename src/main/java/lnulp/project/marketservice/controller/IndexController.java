8c157ea17f3f30b76f1c7812df2b58f6e4048fdf
kriho <kriho@DESKTOP-7VJNRFA>
2017-10-03 23:38:39 +0300

    created project server structure

---
+++ src/main/java/lnulp/project/marketservice/controller/IndexController.java
@@ -0,0 +1,15 @@
+package lnulp.project.marketservice.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/")
+	public String index(HttpServletRequest request, Model model) {
+		return("index");
+	}
+}
